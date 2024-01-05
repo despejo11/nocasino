970b690dfb8fbc1a695fef33c5761702ef8c0b9d
despejo11 <stepanciocoboc@gmail.com>
2024-01-05 15:46:26 +0200

    fixed a few bugs, made the menu for registered and non-registered user, also menu is adaptive for all devices

--- src/main/resources/static/JS/menu.js
+++ src/main/resources/static/JS/menu.js
@@ -1,9 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const menuBg = document.getElementById('menu-header-bg')
+	const menuBar = document.getElementById('menu-header-bar')
+	const nav = document.getElementById('menu-header-nav')
+
+	if (menuBar.classList.contains('change-menu-header')) {
+		menuBg.classList.remove('closed')
+		nav.classList.add('open')
+	} else {
+		menuBg.classList.add('closed')
+		nav.classList.remove('open')
+	}
+})
+
 const menuOnClick = () => {
 	const menuBar = document.getElementById('menu-header-bar')
 	const nav = document.getElementById('menu-header-nav')
 	const menuBg = document.getElementById('menu-header-bg')
 
 	menuBar.classList.toggle('change-menu-header')
-	nav.classList.toggle('change-menu-header')
+	nav.classList.toggle('open')
 	menuBg.classList.toggle('change-menu-header-bg')
+
+	if (menuBg.classList.contains('change-menu-header-bg')) {
+		nav.classList.add('open')
+		menuBg.classList.remove('closed')
+	} else {
+		nav.classList.remove('open')
+		menuBg.classList.add('closed')
+	}
 }
